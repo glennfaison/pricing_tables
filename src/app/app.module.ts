df9d8a2be87dbd0af36af9d432e53efee0816286
glennfaison <glennfaison@gmail.com>
2020-05-04 11:58:29 +0100

    savepoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductComponent } from './components/product/product.component';
+import { PricingPlanComponent } from './components/pricing-plan/pricing-plan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductComponent,
+    PricingPlanComponent
   ],
   imports: [
     BrowserModule
