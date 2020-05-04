2544893682a0ed41e576b7bfa382921196eba142
glennfaison <glennfaison@gmail.com>
2020-05-04 14:02:53 +0100

    savepoint

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -9,90 +9,12 @@ export class ProductComponent implements OnInit {
 
   @Input() product: any;
 
-  pricingPlans: any[] = [
-    {
-      monthly: {
-        id: 'Basic',
-        currency: '$',
-        value: 90,
-        duration: 'mo',
-        Memory: '1024MB',
-        Users: 5,
-        Websites: 10,
-        Domains: 10,
-        Bandwidth: 'Unlimited',
-        Support: '24/7',
-      },
-      yearly: {
-        id: 'Basic',
-        currency: '$',
-        value: 90,
-        duration: 'yr',
-        Memory: '1024MB',
-        Users: 5,
-        Websites: 10,
-        Domains: 10,
-        Bandwidth: 'Unlimited',
-        Support: '24/7',
-      },
-    },
-    {
-      monthly: {
-        id: 'Popular',
-        currency: '$',
-        value: 90,
-        duration: 'mo',
-        Memory: '1024MB',
-        Users: 5,
-        Websites: 10,
-        Domains: 10,
-        Bandwidth: 'Unlimited',
-        Support: '24/7',
-      },
-      yearly: {
-        id: 'Popular',
-        currency: '$',
-        value: 90,
-        duration: 'yr',
-        Memory: '1024MB',
-        Users: 5,
-        Websites: 10,
-        Domains: 10,
-        Bandwidth: 'Unlimited',
-        Support: '24/7',
-      },
-    },
-    {
-      monthly: {
-        id: 'Premier',
-        currency: '$',
-        value: 90,
-        duration: 'mo',
-        Memory: '1024MB',
-        Users: 5,
-        Websites: 10,
-        Domains: 10,
-        Bandwidth: 'Unlimited',
-        Support: '24/7',
-      },
-      yearly: {
-        id: 'Premier',
-        currency: '$',
-        value: 90,
-        duration: 'yr',
-        Memory: '1024MB',
-        Users: 5,
-        Websites: 10,
-        Domains: 10,
-        Bandwidth: 'Unlimited',
-        Support: '24/7',
-      },
-    }
-  ];
+  pricingPlans: any[];
 
   constructor() { }
 
   ngOnInit() {
+    this.pricingPlans = this.product.pricingPlans;
   }
 
 }
